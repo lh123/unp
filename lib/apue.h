8cc5caeaf0a7cd9b89f9322c2e044f2c5b125c21
lh123 <1585086582@qq.com>
2019-01-13 19:29:24 +0800

    init

---
+++ lib/apue.h
@@ -0,0 +1,33 @@
+#if !defined(_APUE_H)
+#define _APUE_H
+
+#if !defined(_POSIX_C_SOURCE)
+#define _POSIX_C_SOURCE 200809
+#endif
+
+#define MAXLINE 4096
+
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+void __attribute__((format(printf, 1, 2))) err_ret(const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 1, 2))) err_sys(const char *fmt, ...);
+void __attribute__((format(printf, 2, 3))) err_cont(int error, const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 2, 3))) err_exit(int error, const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 1, 2))) err_dump(const char *fmt, ...);
+void __attribute__((format(printf, 1, 2))) err_msg(const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 1, 2))) err_quit(const char *fmt, ...);
+
+extern int log_to_stderr;
+
+void log_open(const char *ident, int option, int facility);
+void __attribute__((format(printf, 1, 2))) log_ret(const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 1, 2))) log_sys(const char *fmt, ...);
+void __attribute__((format(printf, 2, 3))) log_cont(int error, const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 2, 3))) log_exit(int error, const char *fmt, ...);
+void __attribute__((format(printf, 1, 2))) log_msg(const char *fmt, ...);
+void __attribute__((noreturn, format(printf, 1, 2))) log_quit(const char *fmt, ...);
+
+#endif
---
+++ lib/log.c
@@ -0,0 +1,77 @@
+#include "apue.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/syslog.h>
+
+int log_to_stderr;
+
+static void log_doit(int errnoflag, int error, int priority, const char *fmt, va_list ap) {
+    char buf[MAXLINE];
+    vsnprintf(buf, MAXLINE - 1, fmt, ap);
+    if(errnoflag) {
+        snprintf(buf + strlen(buf), MAXLINE - strlen(buf) - 1, ": %s", strerror(error));
+    }
+    strcat(buf, "\n");
+    if (log_to_stderr) {
+        fflush(stdout);
+        fputs(buf, stderr);
+        fflush(stderr);
+    } else {
+        syslog(priority, "%s", buf);
+    }
+}
+
+void log_open(const char *ident, int option, int facility) {
+    if (log_to_stderr == 0) {
+        openlog(ident, option, facility);
+    }
+}
+
+void log_ret(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(1, errno, LOG_ERR, fmt, ap);
+    va_end(ap);
+}
+
+void log_sys(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(1, errno, LOG_ERR, fmt, ap);
+    va_end(ap);
+    exit(2);
+}
+
+void log_cont(int error, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(1, error, LOG_ERR, fmt, ap);
+    va_end(ap);
+}
+
+void log_exit(int error, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(1, error, LOG_ERR, fmt, ap);
+    va_end(ap);
+    exit(2);
+}
+
+void log_msg(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(0, 0, LOG_ERR, fmt, ap);
+    va_end(ap);
+}
+
+void log_quit(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    log_doit(0, 0, LOG_ERR, fmt, ap);
+    va_end(ap);
+    exit(2);
+}
