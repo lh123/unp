cf4e0bbfc7504c65797ec6fac6666c33ecae1907
lh123 <1585086582@qq.com>
2019-01-15 22:21:53 +0800

    fix readline

--- lib/unp.h
+++ lib/unp.h
@@ -8,15 +8,20 @@
 #define MAXLINE 4096
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/wait.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define LISTENQ 8
+#define SERV_PORT 9877
 #define bzero(ptr, n) memset((ptr), 0, (n))
 
 void __attribute__((format(printf, 1, 2))) err_ret(const char *fmt, ...);
@@ -42,5 +47,8 @@ char *sock_ntop(const struct sockaddr *sockaddr, socklen_t addrlen);
 ssize_t readn(int fd, void *ptr, size_t n);
 ssize_t writen(int fd, const void *ptr, size_t n);
 ssize_t readline(int fd, void *ptr, size_t maxlen);
+
+void str_echo(int sockfd);
+void str_cli(FILE *fp, int sockfd);
 
 #endif
